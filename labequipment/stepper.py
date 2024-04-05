ebe59de2a38a7d6a0d371b4db17285defd3f1684
mikesmithlab <mikesmithlab@gmail.com>
2024-04-05 16:44:00 +0100

    update stepper

--- labequipment/stepper.py
+++ labequipment/stepper.py
@@ -8,7 +8,7 @@ class Stepper():
         """ Initialise with an instance of arduino.Arduino"""
         self.ard = ard
 
-    def move_motor(self, motor_no, steps, direction):
+    def move_motor(self, motor_no, steps, direction, steps_per_sec=2000/128):
         """
         Generate the message to be sent to self.ard.send_serial_line
 
@@ -18,14 +18,13 @@ class Stepper():
         direction: either '+' or '-'
         max steps 100000 --> Though it may bottom out etc before that!
         """
-        self.motor_timeout = 1+ 0.065 * abs(steps)
-
+        self.motor_timeout = 2 + (1/steps_per_sec) * abs(steps)
         message = 'M' + str(motor_no) + direction + str(steps) + '\n'
         self.ard.send_serial_line(message)
-        time.sleep(0.2)
+        time.sleep(0.1)
         success = self.get_motor_confirmation()
         return success
-    
+
     def get_motor_confirmation(self):
         """The Arduino Sketch for stepper motors 'Shaker_Motor_v2.ino' is set up
         so that when a message is sent to it to move the motor is replies
@@ -38,22 +37,17 @@ class Stepper():
         
         This function will return success=True if it received confirmation that the motor has moved.        
         """
-        
-
-        _ = self.ard.read_serial_line() #Throw away initial new line char
-        _ = self.ard.read_serial_line() #Arduino reflects instruction
-
+         
         # Wait for bytes in the serial buffer until the timeout
         tic = time.time()
-        while (self.ard.port.inWaiting() == 0):  
+        while True:
             toc = time.time()
+            if self.ard.port.inWaiting() > 0:
+                reply = self.ard.read_serial_line()  # Read the bytes
+                if 'moved' in reply:
+                    return True
             if toc - tic > self.motor_timeout:
-                break
+                return False
             time.sleep(0.1)
-        second_reply = self.ard.read_serial_line()  # Read the bytes
-        if 'moved' in second_reply:
-            return True
-        else:
-            return False
         
 
